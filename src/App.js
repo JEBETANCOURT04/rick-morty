88aa93bf0a720362fe106edf31e32f9aed877be2
draken <jebetancourt61@misena.edu.co>
2023-02-24 12:55:08 -0500

    Replanted view detail and refactor loading component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import InitialPage from './components/InitialPage';
 import FetchResults from './components/FetchResults';
-import CharacterDetails from './components/CharacterDetails';
-//import FetchSigleCharacter from './services/FetchSigleCharacter';
+
 
 
 function App() {      
@@ -13,9 +12,7 @@ function App() {
       <BrowserRouter>
         <Routes>          
           <Route path='/' element={<InitialPage />} />
-          <Route path='/results' element={<FetchResults />} />  
-          {/* <Route path='/modalChar' element={<ModalCharacter />} /> */}
-          <Route path='/character/:id' element={<CharacterDetails />} />
+          <Route path='/results' element={<FetchResults />} />            
         </Routes>
       </BrowserRouter>    
     </div>
--- src/services/FetchAllCharacters.jsx
+++ src/services/FetchAllCharacters.jsx
@@ -1,49 +1,52 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { GiDeathSkull, GiHeartBeats } from "react-icons/gi";
+import LoadCard from "../components/LoadCard";
 
-const FetchAllCharacters = () => {  
-
+const FetchAllCharacters = () => {
   const [allCharacter, setAllCharacter] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    console.log("Fetching all characters");
-    fetch("https://rickandmortyapi.com/api/character")
+    setLoading(true);    
+    setTimeout(() => {
+      fetch("https://rickandmortyapi.com/api/character")
       .then((response) => response.json())
-      .then((data) => setAllCharacter(data.results));
-    return () => {};
+      .then((data) => {
+        setAllCharacter(data.results);
+        setLoading(false);
+      });
+    }, 1500)
   }, []);
+
+  if (loading) {
+    return (
+      <LoadCard />
+    );
+  }
 
   return (
     <div className="container max-w-full flex place-items-center px-4 flex-wrap justify-evenly gap-y-2">
-      {allCharacter.map((character) => (
-        <Link key={character.id} to={`/character/${character.id}`}>
+      {allCharacter.map((character) => (        
           <div
             key={character.id}
-            className="w-64 flex justify-center flex-col items-center border-solid border-4 border-grey px-4 py-4"
+            className="w-72 h-96 flex justify-center flex-col items-center border-solid border-4 border-grey px-4 py-4"
           >
-            {character.image ? (
-              <img src={character.image} alt="" />
-            ) : (
-              <h1>Item Desconocido</h1>
-            )}
-            {!character.name ? (
-              <h3>item Desconocido</h3>
-            ) : (
-              <h3>{character.name}</h3>
-            )}
-            {/* {character.origin == "unknown" ? (
-            <h3>item Desconocido</h3>
-          ) : (
-            <h4>{character.origin}</h4>
-          )}
-          <h4>{character.location}</h4>
-          <h4>{character.status}</h4>
-          <h4>{character.id}</h4> */}
-          </div>
-        </Link>
+            <img className="w-3/4 mt-0.5" src={character.image} alt="" />
+            <h3 className="mt-1.5">{character.name}</h3>
+            {character.status === "Alive" ? <GiHeartBeats /> : <GiDeathSkull />}
+            <div className="text-center">
+              <h4>Specie: {character.species}</h4>
+              <h4>Gender: {character.gender}</h4>
+              <h4>Origin: {character.origin.name}</h4>
+              <h4>Location: {character.location.name}</h4>
+            </div>
+          </div>        
       ))}
     </div>
   );
 };
 
 export default FetchAllCharacters;
+
+// return () => {}
